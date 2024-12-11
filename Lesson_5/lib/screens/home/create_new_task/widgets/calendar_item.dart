42a0ba580d0768f2baef3ffdcd68c1029d4e1190
thongnt78 <minhthongnt2610@gmail.com>
2024-12-11 16:39:36 +0700

    lesson8

--- Lesson_5/lib/screens/home/create_new_task/widgets/calendar_item.dart
+++ Lesson_5/lib/screens/home/create_new_task/widgets/calendar_item.dart
@@ -1,74 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:lesson_5/extensions/date_time_extension.dart';
 
 import '../../../../constants/app_colors.dart';
 
 class CalendarItem extends StatelessWidget {
   const CalendarItem({
     super.key,
-    required this.day,
-    required this.weekDay,
+    required this.date,
     required this.isSelected,
+    required this.onSelected,
   });
-  final String weekDay;
-  final String day;
+  final DateTime date;
   final bool isSelected;
+  final VoidCallback onSelected;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(8),
+    return GestureDetector(
+      onTap: () {
+        onSelected.call();
+      },
       child: Stack(
         alignment: Alignment.bottomCenter,
         children: [
-          Container(
-            width: 58,
-            height: 70,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(
-                color: isSelected ? AppColors.hexBA83DE : Colors.transparent,
-                width: 2,
-              ),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  weekDay,
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: isSelected
-                        ? AppColors.hexBA83DE
-                        : Colors.white.withOpacity(0.6),
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 6,
-                ),
-                Text(
-                  day,
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: isSelected
-                        ? AppColors.hexBA83DE
-                        : Colors.white.withOpacity(0.6),
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          ),
           Positioned(
-            bottom: -3,
+            bottom: 3,
             child: Container(
-              width: 8,
-              height: 8,
+              width: 58,
+              height: 68,
               decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(4)),
-                color: isSelected ? AppColors.hexBA83DE : Colors.transparent,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: isSelected ? AppColors.hexBA83DE : Colors.transparent,
+                  width: 2,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    date.displayWeekDaysOfString(),
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: isSelected
+                          ? AppColors.hexBA83DE
+                          : Colors.white.withOpacity(0.6),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    date.displayDaysOfString(),
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: isSelected
+                          ? AppColors.hexBA83DE
+                          : Colors.white.withOpacity(0.6),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
+          if (isSelected)
+            Positioned(
+              bottom: 0,
+              child: Container(
+                width: 8,
+                height: 8,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(4)),
+                  color: AppColors.hexBA83DE,
+                ),
+              ),
+            ),
         ],
       ),
     );
--- Lesson_5/lib/screens/home/create_new_task/widgets/set_date.dart
+++ Lesson_5/lib/screens/home/create_new_task/widgets/set_date.dart
@@ -1,72 +1,157 @@
 import 'package:flutter/material.dart';
+import 'package:lesson_5/constants/app_icons.dart';
+import 'package:lesson_5/extensions/date_time_extension.dart';
+import 'package:lesson_5/screens/home/create_new_task/widgets/week_calendar.dart';
 
 import '../../../../constants/app_colors.dart';
-import 'calendar_item.dart';
 
 class SetDate extends StatefulWidget {
   const SetDate({
     super.key,
+    required this.date,
+    required this.selectedDate,
+    required this.onDateChanged,
   });
+  final DateTime date;
+  final DateTime selectedDate;
+  final ValueChanged<DateTime> onDateChanged;
 
   @override
   State<SetDate> createState() => _setDate();
 }
 
 class _setDate extends State<SetDate> {
-  final List<String> weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sun'];
-  late String? selectedDate = '';
+  DateTime _date = DateTime.now();
+  List<DateTime> _dates = [];
+  DateTime? _selectedDate;
+  final _initalPage = 520;
+  late PageController _pageController;
+  late int _currentPage;
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _initalPage);
+    _date = widget.date;
+    _dates = [_date.previousWeek, _date, _date.nextWeek];
+    _currentPage = _initalPage;
+    _selectedDate = widget.selectedDate;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        const SizedBox(
+          /// Khoảng cách theo chiều dọc
+          height: 16,
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.arrow_back_ios_new),
-              color: AppColors.hexBA83DE,
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  _currentPage--;
+                  _pageController.animateToPage(
+                    _currentPage,
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  );
+                });
+              },
+              child: SizedBox(
+                width: 44,
+                height: 44,
+                child: Center(
+                  child: Image.asset(
+                    AppIcons.leftArrow,
+                    height: 17,
+                    // height: 8.48,
+                  ),
+                ),
+              ),
             ),
-            const Text(
-              '5 Mar - 9 Mar',
-              style: TextStyle(
+            Text(
+              '${_date.startOfWeek.displayDatesOfString()} - ${_date.endOfWeek.displayDatesOfString()}',
+              style: const TextStyle(
                 fontWeight: FontWeight.w400,
                 fontSize: 20,
                 color: AppColors.hexBA83DE,
               ),
             ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.arrow_forward_ios),
-              color: AppColors.hexBA83DE,
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  _currentPage++;
+                  _pageController.animateToPage(
+                    _currentPage,
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  );
+                });
+              },
+              child: SizedBox(
+                width: 44,
+                height: 44,
+                child: Center(
+                  child: Image.asset(
+                    AppIcons.rightArrow,
+                    height: 17,
+                    // height: 8.48,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
+        const SizedBox(
+          height: 25,
+        ),
         SizedBox(
-          // width: 58,
-          height: 70,
-          child: ListView.builder(
-            shrinkWrap: true,
-            scrollDirection: Axis.horizontal,
-            // itemCount: weekDays.length,
+          height: 100,
+          child: PageView.builder(
+            controller: _pageController,
+            onPageChanged: _onPageChanged,
             itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () {
+              return WeekCalendar(
+                dates: _dates[index % _dates.length].daysPerWeek,
+                selectedDate: _selectedDate,
+                onSelected: (date) {
                   setState(() {
-                    selectedDate = '${index = index % 31}';
+                    _selectedDate = date;
                   });
                 },
-                child: CalendarItem(
-                  day: '${index = index % 31 + 1}',
-                  weekDay: weekDays[index % weekDays.length],
-                  isSelected: '${index = index % 31 + 1}' == selectedDate,
-                ),
               );
             },
           ),
         ),
       ],
     );
+  }
+
+  void _onPageChanged(int index) {
+    final page = index % _dates.length;
+    if (page == 0) {
+      setState(() {
+        _date = _dates[0];
+        _dates[2] = _date.previousWeek;
+        _dates[1] = _date.nextWeek;
+        _currentPage = index;
+      });
+    } else if (page == 2) {
+      setState(() {
+        _date = _dates[2];
+        _dates[0] = _date.nextWeek;
+        _dates[1] = _date.previousWeek;
+        _currentPage = index;
+      });
+    } else if (page == 1) {
+      setState(() {
+        _date = _dates[1];
+        _dates[0] = _date.previousWeek;
+        _dates[2] = _date.nextWeek;
+        _currentPage = index;
+      });
+    }
   }
 }
