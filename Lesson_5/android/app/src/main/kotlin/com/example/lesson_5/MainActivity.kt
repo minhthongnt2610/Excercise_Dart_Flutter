37a85755ba23dbd8a9bbf3da7273d2d4e48731ed
thongnt78 <minhthongnt2610@gmail.com>
2024-12-19 20:29:23 +0700

    lesson10

--- Lesson_5/android/app/src/main/kotlin/com/example/lesson_5/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lesson_5
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Lesson_5/lib/constants/app_images.dart
+++
@@ -1,4 +0,0 @@
-class AppImages {
-  AppImages._private();
-  static const avatar = "assets/images/avatar.jpg";
-}
